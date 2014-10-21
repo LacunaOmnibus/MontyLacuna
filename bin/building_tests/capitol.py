bb49718841e0796d272c000bb09e166547819031
tmtowtdi <tmtowtdi@gmail.com>
2014-10-21 16:27:06 -0400

    Added capitol.py

---
+++ bin/building_tests/capitol.py
@@ -0,0 +1,23 @@
+
+import os, sys
+bindir = os.path.abspath(os.path.dirname(__file__))
+libdir = bindir + "/../../lib"
+sys.path.append(libdir)
+
+import lacuna as lac
+
+glc = lac.clients.Member(
+    config_file = bindir + "/../../etc/lacuna.cfg",
+    config_section = 'my_sitter',
+)
+
+my_planet = glc.get_body_byname( 'bmots rof 2.1' )
+cap = my_planet.get_building_coords( 0, -3 )
+
+
+### Get available actions
+###
+rva = cap.rename_empire( "tmtowtdi" )
+glc.pp.pprint( rva )
+
+
--- lib/lacuna/buildings/capitol.py
+++ lib/lacuna/buildings/capitol.py
@@ -1,4 +1,5 @@
 
+from lacuna.bc import LacunaObject
 from lacuna.building import Building
 
 class capitol(Building):
@@ -6,3 +7,17 @@ class capitol(Building):
 
     def __init__( self, client, body_id:int = 0, building_id:int = 0 ):
         super().__init__( client, body_id, building_id )
+
+    @LacunaObject.set_empire_status
+    @Building.call_building_meth
+    def rename_empire( self, name:str, **kwargs ):
+        """ Renames your empire.
+
+        Renaming your empire has a base cost of 30E, but that cost is reduced 
+        by 1E per level of the capitol.  So at Capitol level 30, the rename 
+        process has no cost.
+
+        Throws ServerError 1010 if you attempt to rename your empire more 
+        than once in a 24 hour period.
+        """
+        pass
