083b14387249f8f734461c2e5dd1736aad8729d1
tmtowtdi <tmtowtdi@gmail.com>
2014-10-23 15:29:25 -0400

    Added libraryofjith.py

---
+++ bin/building_tests/libraryofjith.py
@@ -0,0 +1,38 @@
+
+import os, sys
+bindir = os.path.abspath(os.path.dirname(__file__))
+libdir = bindir + "/../../lib"
+sys.path.append(libdir)
+
+import lacuna as lac
+from lacuna.exceptions import CaptchaResponseError
+
+glc = lac.clients.Member(
+    config_file = bindir + "/../../etc/lacuna.cfg",
+    #config_section = 'my_sitter',
+    config_section = 'play_test',
+)
+my_planet   = glc.get_body_byname( 'bmots rof 2.6' )
+lib         = my_planet.get_building_coords( -2, 1 )
+
+
+### Get information on a specific empire's species
+###
+empire_to_research = 'tmtowtdi'
+
+### First, find that empire's ID:
+stats = glc.get_stats()
+emps = stats.find_empire_rank( '', empire_to_research )['empires']
+emp = {}
+for i in emps:
+    if i['empire_name'] == empire_to_research:
+        emp = i
+if not 'empire_id' in emp:
+    raise AttributeError("Could not find the desired empire.")
+print( "The ID of the empire you're looking for is:", emp['empire_id'] )
+
+### Now that we have the desired empire ID, we can check its species.
+rva = lib.research_species( emp['empire_id'] )
+glc.pp.pprint( rva['species'] )
+
+
--- lib/lacuna/buildings/libraryofjith.py
+++ lib/lacuna/buildings/libraryofjith.py
@@ -1,4 +1,5 @@
 
+from lacuna.bc import LacunaObject
 from lacuna.building import Building
 
 class libraryofjith(Building):
@@ -6,3 +7,30 @@ class libraryofjith(Building):
 
     def __init__( self, client, body_id:int = 0, building_id:int = 0 ):
         super().__init__( client, body_id, building_id )
+
+
+    @LacunaObject.set_empire_status
+    @Building.call_building_meth
+    def research_species( self, empire_id:int, *args, **kwargs ):
+        """ Returns information on the species of the indicated empire.
+
+        Retval includes 'species', a dict:
+                {
+                    "name" : "Human",
+                    "description" : "The descendants of Earth.",
+                    "min_orbit" : 3,
+                    "max_orbit" : 3,
+                    "manufacturing_affinity" : 4,
+                    "deception_affinity" : 4,
+                    "research_affinity" : 4,
+                    "management_affinity" : 4,
+                    "farming_affinity" : 4,
+                    "mining_affinity" : 4,
+                    "science_affinity" : 4,
+                    "environmental_affinity" : 4,
+                    "political_affinity" : 4,
+                    "trade_affinity" : 4,
+                    "growth_affinity" : 4
+                },
+        """
+        pass
