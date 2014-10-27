f49dc8104807bc60d1c4c27825d92baf91f4889c
tmtowtdi <tmtowtdi@gmail.com>
2014-10-27 16:20:01 -0400

    Added missioncommand.py

--- bin/building_tests/miningministry.py
+++ bin/building_tests/miningministry.py
@@ -42,54 +42,21 @@ minmin      = my_planet.get_building_coords( -3, 3 )
 
 ### Remove a cargo ship to the current mining fleet
 ###
-rvd = minmin.view_ships()
-for s in rvd['ships']:
-    if s['task'] == 'Mining' and re.match("Hulk Fast", s['name']):
-        minmin.remove_cargo_ship_from_fleet( s['id'] )
-        print( "I just removed ship {} (id {}) from your mining fleet.".format(s['name'], s['id']) )
-        break
+#rvd = minmin.view_ships()
+#for s in rvd['ships']:
+#    if s['task'] == 'Mining' and re.match("Hulk Fast", s['name']):
+#        minmin.remove_cargo_ship_from_fleet( s['id'] )
+#        print( "I just removed ship {} (id {}) from your mining fleet.".format(s['name'], s['id']) )
+#        break
 
 
 ### Abandon a mining platform
 ###
-rve = minmin.view_platforms()
-for i in rve['platforms']:
-    if 'asteroid' in i and i['asteroid']['name'] == 'Su Bootto Zoa 5':
-        j = i['asteroid']
-        minmin.abandon_platform( i['id'] )
-        print( "I just abandoned the platform at {} (ID {}).".format(j['name'], j['id']) )
-        break
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#rve = minmin.view_platforms()
+#for i in rve['platforms']:
+#    if 'asteroid' in i and i['asteroid']['name'] == 'Su Bootto Zoa 5':
+#        j = i['asteroid']
+#        minmin.abandon_platform( i['id'] )
+#        print( "I just abandoned the platform at {} (ID {}).".format(j['name'], j['id']) )
+#        break
 
