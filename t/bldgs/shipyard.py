a35d8aa6270ff76f8bad413c69a5e4ee660709a3
tmtowtdi <tmtowtdi@gmail.com>
2014-10-28 15:46:28 -0400

    added shipyard.py

--- t/bldgs/shipyard.py
+++ t/bldgs/shipyard.py
@@ -12,15 +12,53 @@ glc = lac.clients.Member(
     config_section = 'play_test',
 )
 my_planet   = glc.get_body_byname( 'bmots rof 2.1' )
-sy          = my_planet.get_building_coords( 3, -4 )
+sy          = my_planet.get_building_coords( 4, -4 )
 
 
-### View prisoners
+### View build queue
 ###
-pris = sy.view_prisoners()
-for i in pris:
-    print( "Prisoner named {} has an ID of {}.".format(pris.name, pris.id) )
+#ships, number, cost = sy.view_build_queue()
+#for i in ships:
+#    print( i.type_human )
+#print( number, "ships are building.")
+#print( "It'll cost", cost, "E to subsidize them.")
 
 
+### Subsidize whole build queue
+###
+#rv = sy.subsidize_build_queue()
+#del rv['status']
+#glc.pp.pprint( rv )
+
+
+### Subsidize a single ship in the queue
+### To test, add a few supply pods to the queue (they're slow to build), then 
+### add an excavator after the pods.
+###
+#ships, number, cost = sy.view_build_queue()
+#ship_id = 0
+#for i in ships:
+#    print( "Got", i.type_human )
+#    if i.type_human == 'Excavator':
+#        print("Subsidizing this one.")
+#        ship_id = i.id
+#        break
+#sy.subsidize_ship( {'ship_id': ship_id} )
+
+
+### Get buildable ships
+###
+#ships, docks, q_max, q_used = sy.get_buildable( 'Trade' )
+#for i in ships:
+#    print( i.type )
+#print( docks, "docks are available for new ships.")
+#print( "We have", q_used, "of a maximum", q_max, "ships queued.")
+
+
+### Build some ships
+###
+#ships, number, cost = sy.build_ship('excavator', 2)
+#print( number, "ships have been added to the queue.")
+#print( "It'll cost", cost, "E to subsidize the entire queue.")
 
 
