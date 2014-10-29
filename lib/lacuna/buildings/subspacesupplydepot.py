aec0d91daff4cc0d22a44e054dc27eb55a6e5c91
tmtowtdi <tmtowtdi@gmail.com>
2014-10-29 18:16:22 -0400

    Added templeofthedrajilites.py

--- lib/lacuna/buildings/subspacesupplydepot.py
+++ lib/lacuna/buildings/subspacesupplydepot.py
@@ -1,8 +1,53 @@
 
+from lacuna.bc import LacunaObject
 from lacuna.building import Building
+
+"""
+    This entire module is untested; I haven't got an SSD to fool with.
+
+    Since a utility script to run an SSD wouldn't have much utility, I'm not 
+    going to stress over this too much.
+
+
+    Each method in here should return the same thing, a retval containing the 
+    key 'building':
+            {
+                "work" : {
+                    "seconds_remaining" : 99,
+                    "start" : "01 31 2010 13:09:05 +0600",
+                    "end" : "01 31 2010 13:09:05 +0600"
+                }
+            },
+
+"""
 
 class subspacesupplydepot(Building):
     path = 'subspacesupplydepot'
 
     def __init__( self, client, body_id:int = 0, building_id:int = 0 ):
         super().__init__( client, body_id, building_id )
+
+    @LacunaObject.set_empire_status
+    @Building.call_building_meth
+    def transmit_food( self, *args, **kwargs ):
+        pass
+
+    @LacunaObject.set_empire_status
+    @Building.call_building_meth
+    def transmit_energy( self, *args, **kwargs ):
+        pass
+
+    @LacunaObject.set_empire_status
+    @Building.call_building_meth
+    def transmit_ore( self, *args, **kwargs ):
+        pass
+
+    @LacunaObject.set_empire_status
+    @Building.call_building_meth
+    def transmit_water( self, *args, **kwargs ):
+        pass
+
+    @LacunaObject.set_empire_status
+    @Building.call_building_meth
+    def complete_build_queue( self, *args, **kwargs ):
+        pass
---
+++ t/bldgs/subspacesupplydepot.py
@@ -0,0 +1,20 @@
+
+import os, re, sys
+bindir = os.path.abspath(os.path.dirname(__file__))
+libdir = bindir + "/../../lib"
+sys.path.append(libdir)
+
+import lacuna as lac
+
+glc = lac.clients.Member(
+    config_file = bindir + "/../../etc/lacuna.cfg",
+    #config_section = 'my_sitter',
+    config_section = 'play_test',
+)
+my_planet   = glc.get_body_byname( 'bmots rof 2.1' )
+
+
+### This is completely untested.  I haven't got an SSD lying around to play 
+### with.
+
+
