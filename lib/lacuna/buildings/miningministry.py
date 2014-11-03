27ca5f5643a7fdc04c2ee2acaca9beebb9fc3c5a
tmtowtdi <tmtowtdi@gmail.com>
2014-11-03 17:31:20 -0500

    oopified mining ministry

--- lib/lacuna/buildings/miningministry.py
+++ lib/lacuna/buildings/miningministry.py
@@ -1,20 +1,98 @@
 
-from lacuna.bc import LacunaObject
-from lacuna.building import MyBuilding
+import lacuna.bc
+import lacuna.building
+import lacuna.ship
 
-class miningministry(MyBuilding):
+class miningministry(lacuna.building.MyBuilding):
     path = 'miningministry'
 
     def __init__( self, client, body_id:int = 0, building_id:int = 0 ):
         super().__init__( client, body_id, building_id )
 
-    @LacunaObject.set_empire_status
-    @MyBuilding.call_building_meth
+    @lacuna.building.MyBuilding.call_returning_meth
     def view_platforms( self, *args, **kwargs ):
         """ Views your current platform status
 
-        Retval includes:
-            "max_platforms":    Integer max platforms this min min can support
+        Returns a tuple:
+            platforms       List of MiningPlatform objects
+            max_platforms   Integer max platforms this min min can support
+        """
+        mylist = []
+        for i in kwargs['rslt']['platforms']:
+            mylist.append( MiningPlatform(self.client, i) )
+        return (
+            mylist,
+            kwargs['rslt']['max_platforms']
+        )
+
+    @lacuna.building.MyBuilding.call_returning_meth
+    def view_ships( self, *args, **kwargs ):
+        """ View list of mining-capable ships.
+
+        All ships that can be used for mining are returned.  If a given ship's 
+        task is 'Mining', it's currently shutting ore to and from your mining 
+        platforms.  If its task is listed as 'Docked', it's available to be 
+        added to your current mining fleet.
+
+        Retval includes 'ships', a list of ship dicts:
+                {
+                    "name" : "CS4",
+                    "id" : "id-goes-here",
+                    "task" : "Mining",
+                    "speed" : 350,
+                    "hold_size" : 5600
+                },
+        """
+        mylist = []
+        for i in kwargs['rslt']['ships']:
+            mylist.append( lacuna.ship.ChainShip(self.client, i) )
+        return mylist
+
+    @lacuna.bc.LacunaObject.set_empire_status
+    @lacuna.building.MyBuilding.call_building_meth
+    def add_cargo_ship_to_fleet( self, ship_id:int, *args, **kwargs ):
+        """ Adds a ship to the mining fleet.
+
+        The "cargo ship" does not have to specifically be of type "cargo_ship", 
+        it just has to be capable of carrying cargo (eg hulk_mega, 
+        smuggler_ship, etc).  
+
+        Requires "ship_id", the integer ID of the ship to add to the fleet.
+        """
+        pass
+
+    @lacuna.bc.LacunaObject.set_empire_status
+    @lacuna.building.MyBuilding.call_building_meth
+    def remove_cargo_ship_from_fleet( self, ship_id:int, *args, **kwargs ):
+        """ Takes a single ship off mining duty; sends the ship a message to 
+        return to base to perform the "Docked" task.
+
+        After being removed from mining duty, the ship will need to travel 
+        from the mining location back to your space port's planet, so it will 
+        not be available for use immediately.
+
+        Requires "ship_id", the integer ID of the ship to removed from the 
+        fleet.
+        """
+        pass
+
+    @lacuna.bc.LacunaObject.set_empire_status
+    @lacuna.building.MyBuilding.call_building_meth
+    def abandon_platform( self, platform_id:int, *args, **kwargs ):
+        """ Abandon one of your mining platforms.
+
+        Requires "platform_id", the integer ID of the platform to abandon.
+
+        Remember that you might have multiple platforms on a single asteroid.  
+        When you abandon, you are abandoning the platform, not the asteroid, so 
+        be careful to send the platform ID, not the ID of the asteroid itself.
+        """
+        pass
+
+
+class MiningPlatform(lacuna.bc.SubClass):
+    """
+
             "platforms":        List of platform dicts
                         {
                             "id" : "id-goes-here",
@@ -48,69 +126,6 @@ class miningministry(MyBuilding):
                             "magnetite_hour" : 10,  
                             "shipping_capacity" : 51 
                         },
-        """
-        pass
 
-    @LacunaObject.set_empire_status
-    @MyBuilding.call_building_meth
-    def view_ships( self, *args, **kwargs ):
-        """ View list of mining-capable ships.
-
-        All ships that can be used for mining are returned.  If a given ship's 
-        task is 'Mining', it's currently shutting ore to and from your mining 
-        platforms.  If its task is listed as 'Docked', it's available to be 
-        added to your current mining fleet.
-
-        Retval includes 'ships', a list of ship dicts:
-                {
-                    "name" : "CS4",
-                    "id" : "id-goes-here",
-                    "task" : "Mining",
-                    "speed" : 350,
-                    "hold_size" : 5600
-                },
-        """
-        pass
-
-    @LacunaObject.set_empire_status
-    @MyBuilding.call_building_meth
-    def add_cargo_ship_to_fleet( self, ship_id:int, *args, **kwargs ):
-        """ Adds a ship to the mining fleet.
-
-        The "cargo ship" does not have to specifically be of type "cargo_ship", 
-        it just has to be capable of carrying cargo (eg hulk_mega, 
-        smuggler_ship, etc).  
-
-        Requires "ship_id", the integer ID of the ship to add to the fleet.
-        """
-        pass
-
-    @LacunaObject.set_empire_status
-    @MyBuilding.call_building_meth
-    def remove_cargo_ship_from_fleet( self, ship_id:int, *args, **kwargs ):
-        """ Takes a single ship off mining duty; sends the ship a message to 
-        return to base to perform the "Docked" task.
-
-        After being removed from mining duty, the ship will need to travel 
-        from the mining location back to your space port's planet, so it will 
-        not be available for use immediately.
-
-        Requires "ship_id", the integer ID of the ship to removed from the 
-        fleet.
-        """
-        pass
-
-    @LacunaObject.set_empire_status
-    @MyBuilding.call_building_meth
-    def abandon_platform( self, platform_id:int, *args, **kwargs ):
-        """ Abandon one of your mining platforms.
-
-        Requires "platform_id", the integer ID of the platform to abandon.
-
-        Remember that you might have multiple platforms on a single asteroid.  
-        When you abandon, you are abandoning the platform, not the asteroid, so 
-        be careful to send the platform ID, not the ID of the asteroid itself.
-        """
-        pass
-
+    """
 
