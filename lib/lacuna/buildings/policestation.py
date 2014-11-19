7ec21676422f349046964e6e06729a0a9516e9fa
tmtowtdi <tmtowtdi@gmail.com>
2014-11-18 21:42:03 -0500

    started policestation

--- lib/lacuna/buildings/policestation.py
+++ lib/lacuna/buildings/policestation.py
@@ -1,8 +1,24 @@
 
-from lacuna.building import MyBuilding
+import lacuna.bc
+import lacuna.building
+import lacuna.ship
 
-class policestation(MyBuilding):
+class policestation(lacuna.building.MyBuilding):
     path = 'policestation'
 
     def __init__( self, client, body_id:int = 0, building_id:int = 0 ):
         super().__init__( client, body_id, building_id )
+
+    @lacuna.building.MyBuilding.call_returning_meth
+    def view_foreign_ships( self, page_number:int = 1, **kwargs ):
+        """ Shows incoming foreign ships, 25 at a time.
+
+        Arguments:
+            - page_number -- the page of results to view
+
+        Returns a list of ship.IncomingShip objects.
+        """
+        mylist = []
+        for i in kwargs['rslt']['ships']:
+            mylist.append( lacuna.ship.IncomingShip(self.client, i) )
+        return mylist
