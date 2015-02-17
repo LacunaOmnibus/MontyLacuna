a1e1224c36a75d555971bfaf81f481d4e953e387
tmtowtdi <tmtowtdi@gmail.com>
2015-02-16 21:13:38 -0500

    added influence report to stations_report.py

--- bin/stations_report.py
+++ bin/stations_report.py
@@ -13,17 +13,9 @@ import lacuna.binutils.libstations_report as lib
 sr  = lib.StationsReport()
 l   = sr.client.user_logger
 
-### sr.planets will be a list, containing either just the planet name passed 
-### in by the user, or all of the user's planet names if 'all' was passed in.
+num = 0
 for sname in sorted(sr.stations):
-
-    ### Set the current planet name as our 'working' planet
     sr.set_station( sname )
-
-    ### Display reports on stations with low resource income.  Stations with 
-    ### all 4 resources in positive hourly produce no output at all.
-    #sr.show_low_res( )
-
-    sr.show_all( )
-
+    num += sr.run_report()
+print( "Reported on {} total stations.".format(num) )
 
--- lib/lacuna/binutils/libstations_report.py
+++ lib/lacuna/binutils/libstations_report.py
@@ -18,43 +18,30 @@ class StationsReport(lacuna.binutils.libbin.Script):
         ### CHECK I updated no help docu below.
         parser = argparse.ArgumentParser(
             description = 'Displays a report on one or more space stations.',
-            epilog      = "Full docs can be found at http://tmtowtdi.github.io/MontyLacuna/scripts/stations_report.html",
+            epilog      = "Full docs, including explanations of the different report types, can be found at http://tmtowtdi.github.io/MontyLacuna/scripts/stations_report.html",
         )
         parser.add_argument( 'name', 
-            metavar     = '<planet>',
+            metavar     = '<station>',
             action      = 'store',
-            help        = "Produce report on this station.  'all' to report on all station."
+            help        = "Produce report on this station.  'all' to report on all stations."
         )
         parser.add_argument( '--fresh', 
             action      = 'store_true',
             help        = "Ship data is cached so you can run the report multiple times, quickly.  But if you run it once, then go build ships and want a new report that includes those ships, you'll want fresh, not cached data.  In that case, pass this option on the second run."
         )
-        parser.add_argument( '--format', 
-            metavar     = '<format>',
+        parser.add_argument( '--report', 
+            metavar     = '<report type>',
             action      = 'store',
-            choices     = [ 'cli', 'csv' ],
-            default     = 'cli',
-            help        = "What format do you want your output?  Choices include 'cli' and 'csv'.  Defaults to 'cli'."
-        )
-        parser.add_argument( '--tag', 
-            metavar     = '<tag>',
-            dest        = 'tags',
-            action      = 'append',
-            choices     = [ 'Colonization', 'Exploration', 'Intelligence', 'Mining', 'Trade', 'War' ],
-            type        = str,
-            default     = [],
-            help        = "Report on ships with this tag.  Omit to report on all tags.  Valid tags are: 'Colonization', 'Exploration', 'Intelligence', 'Mining', 'Trade', 'War'."
+            choices     = [ 'allres', 'inf', 'lowres' ],
+            default     = 'allres',
+            help        = "Determines which report to display.  One of 'allres', 'inf', 'lowres'.  Defaults to 'allres'."
         )
         super().__init__(parser)
-
-        self.header_written = False
         self.station        = ''
         self.stations       = []
-
         if self.args.fresh:
             self.client.cache_clear( 'stations_report' )
             self.client.cache_clear( 'my_stations' )
-
         self._set_stations()
 
 
@@ -80,25 +67,45 @@ class StationsReport(lacuna.binutils.libbin.Script):
         self.station = self.client.get_body_byname( sname )
         self.client.cache_off()
 
-    def show_low_res( self ):
+
+    def run_report( self ):
+        if self.args.report == 'lowres':
+            return self.show_lowres()
+        elif self.args.report == 'allres':
+            return self.show_allres()
+        elif self.args.report == 'inf':
+            return self.show_influence()
+
+    def _show_report_header( self ):
+        print( "{} ({}, {}) - Zone {}"
+            .format(self.station.name, self.station.x, self.station.y, self.station.zone)
+        )
+
+    def show_lowres( self ):
+        cnt = 0
         if self.station.food_hour < 0 or self.station.ore_hour < 0 or self.station.water_hour < 0 or self.station.energy_hour < 0:
-            print( "{} ({}, {}) - Zone {}"
-                .format(self.station.name, self.station.x, self.station.y, self.station.zone)
-            )
+            cnt += 1
+            self._show_report_header()
             print( "\tFood / hour:", self.station.food_hour )
             print( "\tOre / hour:", self.station.ore_hour )
             print( "\tWater / hour:", self.station.water_hour )
             print( "\tEnergy / hour:", self.station.energy_hour )
             print( "---------------" )
+        return cnt
 
-    def show_all( self ):
-        print( "{} ({}, {}) - Zone {}"
-            .format(self.station.name, self.station.x, self.station.y, self.station.zone)
-        )
+    def show_allres( self ):
+        cnt = 1
+        self._show_report_header()
         print( "\tFood / hour:", self.station.food_hour )
         print( "\tOre / hour:", self.station.ore_hour )
         print( "\tWater / hour:", self.station.water_hour )
         print( "\tEnergy / hour:", self.station.energy_hour )
         print( "---------------" )
+        return cnt
 
+    def show_influence( self ):
+        cnt = 1
+        self._show_report_header()
+        print( "\tSpent/Available -- {}/{}".format(self.station.influence.spent, self.station.influence.total) )
+        return cnt
 
