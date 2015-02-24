7e537759e662c76ee116b2ebeaf1518a5921ed09
tmtowtdi <tmtowtdi@gmail.com>
2015-02-24 18:24:54 -0500

    updated copyright date

--- doc/conf.py
+++ doc/conf.py
@@ -58,7 +58,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Monty Lacuna'
-copyright = '2014, Jonathan Barton'
+copyright = '2014 - 2015, Jonathan Barton'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
